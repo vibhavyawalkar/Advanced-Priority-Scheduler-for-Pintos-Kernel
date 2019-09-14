cfba7fe0092dd5686b19b10343a8223d9e99e06e
Ziyiliu11 <liuzy1994@gmail.com>
2019-09-14 18:37:48 -0400

    Revert "reupload"
    This reverts commit a426ba7f1ebb6027275cdde4b1902b8d517a2b14.
    
    Revert to the origin file

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -4,7 +4,6 @@
 #include <debug.h>
 #include <list.h>
 #include <stdint.h>
-#include "threads/synch.h"
 
 /* States in a thread's life cycle. */
 enum thread_status
@@ -93,9 +92,6 @@ struct thread
 
     /* Shared between thread.c and synch.c. */
     struct list_elem elem;              /* List element. */
-
-    struct semaphore t_sema;
-    int64_t wakeup_time;                /* 64bit integer - wakeup time */
 
 #ifdef USERPROG
     /* Owned by userprog/process.c. */
